6cd7ffaba579b20732a68750c0da92ade07dd280
nikhilljpskj <nikhiljp.skj@gmail.com>
2024-09-25 00:02:46 +0530

    first committ

--- db/db.js
+++ db/db.js
@@ -1,14 +1,13 @@
-const mysql = require('mysql');
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'comsol'
-});
+const mysql = require('mysql2');
+
+// Use your Railway connection URL directly here
+const dbUrl = 'mysql://root:ftFhdeLYRnHiJrkXHipTLKGEweSFqCcc@mysql.railway.internal:3306/railway';
+
+const db = mysql.createConnection(dbUrl);
 
 db.connect((err) => {
   if (err) throw err;
-  console.log('Connected to MySQL');
+  console.log('Connected to MySQL via Railway URL');
 });
 
 module.exports = db;
